55f3f46c293a9f8cf8d9d0e5376b4e5b669a0342
Vincent Perez <perezv815@gmail.com>
2017-03-05 23:07:41 +0100

    Homework 4: Ex3, Ex4

--- homework_4/Homework.hs
+++ homework_4/Homework.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
+import Data.List
 
 -- Ex 1
 fun1' :: [Integer] -> Integer
@@ -27,20 +28,42 @@ isPerfect :: Tree a -> Bool
 isPerfect Leaf               = True
 isPerfect t @ (Node h _ _ _) = nbNodes t == 2 ^ (h + 1) - 1
 
-insert :: a -> Tree a -> Tree a
-insert a Leaf                         = Node 0 Leaf a Leaf
-insert a (Node 0 Leaf b Leaf)         = Node 1 (Node 0 Leaf a Leaf) b Leaf
-insert a (Node h ltree b Leaf)        = Node h ltree b (Node 0 Leaf a Leaf)
-insert a (Node h Leaf b rtree)        = Node h (Node 0 Leaf a Leaf) b rtree
-insert a (Node h ltree @ (Node lh _ _ _) b rtree @ (Node rh _ _ _))
+insertTree :: a -> Tree a -> Tree a
+insertTree a Leaf                         = Node 0 Leaf a Leaf
+insertTree a (Node 0 Leaf b Leaf)         = Node 1 (Node 0 Leaf a Leaf) b Leaf
+insertTree a (Node h ltree b Leaf)        = Node h ltree b (Node 0 Leaf a Leaf)
+insertTree a (Node h Leaf b rtree)        = Node h (Node 0 Leaf a Leaf) b rtree
+insertTree a (Node h ltree @ (Node lh _ _ _) b rtree @ (Node rh _ _ _))
   | lh < rh                           = Node h newltree b rtree
   | lh > rh                           = Node h ltree b newrtree
   | lh == rh && not (isPerfect ltree) = Node h newltree b rtree
   | lh == rh && not (isPerfect rtree) = Node h ltree b newrtree
   | otherwise                         = Node (1 + h) newltree b rtree
   where
-    newltree = insert a ltree
-    newrtree = insert a rtree
+    newltree = insertTree a ltree
+    newrtree = insertTree a rtree
 
 foldTree :: [a] -> Tree a
-foldTree = foldr insert Leaf
+foldTree = foldr insertTree Leaf
+
+-- Ex 3
+-- 1)
+xor :: [Bool] -> Bool
+xor = foldr (/=) False
+
+-- 2)
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x l -> (f x) : l) []
+
+-- Ex 4
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
+f1 :: Integer -> ([Integer], [(Integer, Integer)])
+f1 n = ([1..n], cartProd [1..n] [1..n])
+
+f2 :: ([Integer], [(Integer, Integer)]) -> ([Integer], [Integer])
+f2 c = (fst c, map (\(i, j) -> i + j + 2 * i * j) (snd c))
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram = map ((+1) . (*2)) . (uncurry (\\)) . f2 . f1
