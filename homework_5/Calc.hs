a6c577211cfa925c447a7b1a9aadc750e71c0479
Vincent Perez <perezv815@gmail.com>
2017-03-18 12:01:28 +0100

    Homework 5: Ex3, 4

--- homework_5/Calc.hs
+++ homework_5/Calc.hs
@@ -16,3 +16,42 @@ evalMaybe (Just e) = Just (eval e)
 
 evalStr :: String -> Maybe Integer
 evalStr = evalMaybe . (parseExp Lit Add Mul)
+
+-- Ex 3
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+-- Ex 4
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (>0)
+  add = (||)
+  mul = (&&)
+
+instance Expr MinMax where
+  lit = (MinMax)
+  add (MinMax a) (MinMax b) = MinMax $ max a b
+  mul (MinMax a) (MinMax b) = MinMax $ min a b
+
+instance Expr Mod7 where
+  lit a = Mod7 $ mod a 7
+  add (Mod7 a) (Mod7 b) = Mod7 $ mod (a + b) 7
+  mul (Mod7 a) (Mod7 b) = Mod7 $ mod (a * b) 7
+
+-- tests
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
