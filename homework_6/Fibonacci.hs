eaf42b1cdbc179b6cb52e86429821dce48645d4b
Vincent Perez <perezv815@gmail.com>
2017-04-04 00:02:38 +0200

    Homework 6: Ex1, Ex2

---
+++ homework_6/Fibonacci.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -Wall #-}
+
+-- Ex1
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib(n - 1) + fib(n - 2)
+
+fibs1 :: [Integer]
+fibs1 = map fib [0..]
+
+-- Ex2
+
+fibs2 :: [Integer]
+fibs2 = 0 : 1: zipWith (+) fibs2 (tail(fibs2))
